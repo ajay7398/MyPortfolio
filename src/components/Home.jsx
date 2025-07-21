70095d266d439a4c1f40487a51bc66c7c123d2d9
Ajay kumar harijan <ajaykumar895377@gmail.com>
2025-07-21 12:18:49 +0530

    resume updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
 
       <div className="mt-8 flex gap-4 z-10 flex-col md:flex-row">
         <a
-          href="/MyResume.pdf"
+          href="/Ajay-Resume.pdf"
           target="_blank"
           className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
         >
